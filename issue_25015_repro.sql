4418dc9a2167ba63b5ff00c8a3b11304f2471ffd
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2024-03-15 12:20:09 +0200

    test: simplify failing query

--- issue_25015_repro.sql
+++ issue_25015_repro.sql
@@ -31,21 +31,10 @@ WHERE
           ) AS a1
           JOIN (
             SELECT
-              MIN (
-                a1.f2 + a2.f2 + NULLIF (a2.f1, a2.f2)
-              ) AS f1,
-              COUNT (
-                DISTINCT a2.f2 + NULLIF (a2.f2, a1.f1)
-              ) AS f2
+              0 AS f1,
+              0 AS f2
             FROM
               pk2 AS a1
-              JOIN pk2 AS a2 USING (f1)
-            WHERE
-              a1.f2 + a2.f2 IN (9, 2)
-              AND a1.f1 + NULLIF (a1.f2, a1.f2) NOT IN (0, 7)
-            ORDER BY
-              1,
-              2
           ) AS a2 USING (f1)
         WHERE
           a2.f1 + a1.f2 = a2.f1 + a1.f2
