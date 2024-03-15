b532b23997902426586a2f4abc98926f5f905748
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2024-03-15 12:39:53 +0200

    test: simplify failing query

--- issue_25015_repro.sql
+++ issue_25015_repro.sql
@@ -8,6 +8,4 @@ FROM
 WHERE
   a1.f2 + a2.f2 > (SELECT DISTINCT 0 c2 FROM pk2 AS a1)
   AND a2.f2 IS NULL
-  AND a1.f2 IS NULL
-  AND NULLIF (a2.f2, a1.f1) IS NULL
   OR NULLIF (a2.f2, a2.f1) = a1.f2 + a1.f2;
