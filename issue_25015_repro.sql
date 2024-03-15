4f5ba35178a5bee850dbe3f0707a52d8f0682b52
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2024-03-15 12:24:49 +0200

    test: simplify failing query

--- issue_25015_repro.sql
+++ issue_25015_repro.sql
@@ -6,21 +6,7 @@ FROM
   pk2 AS a1
   JOIN (VALUES(1, 2)) AS a2(f1, f2) ON (a2.f2 = a1.f1)
 WHERE
-  a1.f2 + a2.f2 > (
-    SELECT
-      DISTINCT c2
-    FROM
-      (
-        SELECT
-          COUNT (DISTINCT a1.f2) AS c1,
-          COUNT (a2.f2) AS c2
-        FROM
-          pk2 AS a1
-          JOIN pk2 AS a2 ON (a1.f2 IS NOT NULL)
-        WHERE
-          a1.f2 + NULLIF (a1.f2, a1.f1) IS NULL
-      ) AS dt
-  )
+  a1.f2 + a2.f2 > (SELECT DISTINCT 0 c2 FROM pk2 AS a1)
   AND a2.f2 IS NULL
   AND a1.f2 IS NULL
   AND NULLIF (a2.f2, a1.f1) IS NULL
