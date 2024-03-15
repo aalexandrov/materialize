57011b08d9c781f4e91c98da2610cde2d9c42494
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2024-03-15 12:43:54 +0200

    test: simplify failing query

--- issue_25015_repro.sql
+++ issue_25015_repro.sql
@@ -1,11 +1,9 @@
 SELECT DISTINCT
   (a1.f1) AS c1,
-  (a2.f2 + a1.f2) AS c2,
-  (a1.f2) AS c3
+  (a1.f2) AS c2
 FROM
-  pk2 AS a1
+  t1 AS a1
   JOIN (VALUES(1, 2)) AS a2(f1, f2) ON (a2.f2 = a1.f1)
 WHERE
-  a1.f2 + a2.f2 > (SELECT DISTINCT 0 c2 FROM pk2 AS a1)
-  AND a2.f2 IS NULL
+  a1.f2 + a2.f2 > (SELECT DISTINCT 0 c2 FROM t2 AS a1) AND a2.f2 IS NULL
   OR NULLIF (a2.f2, a2.f1) = a1.f2 + a1.f2;
